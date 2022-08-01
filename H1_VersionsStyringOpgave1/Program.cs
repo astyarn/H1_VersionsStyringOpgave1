c80d39a0b1308a06c32b2c5f0c23f3caff01eba4
Bjoern <bjoe6880@elev.techcollege.dk>
2022-08-01 11:24:09 +0200

    Add project files.

---
+++ H1_VersionsStyringOpgave1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_VersionsStyringOpgave1
+{
+    internal class Program
+    {
+        static int Velkomst()
+        {
+            Console.WriteLine("Velkommen til Lommeregner med Versions control");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("1. Plus");
+            Console.WriteLine("2. Minus");
+            Console.WriteLine("3. Gange");
+            Console.WriteLine("4. Dividere");
+            Console.WriteLine("---------------------");
+            Console.Write("Vælg funktion: ");
+            string input = Console.ReadLine();
+            int heltal = Convert.ToInt32(input);
+            return heltal;
+        }
+
+        static int Plus()
+        {
+            return 0;
+        }
+
+        static int Minus()
+        {
+            return 0;
+        }
+
+        static int Gange()
+        {
+            return 0;
+        }
+
+        static int Dividere()
+        {
+            return 0;
+        }
+
+        static void UdskrivResultat(int temp)
+        {
+            Console.WriteLine("---------------------");
+            Console.Write("Resultat: ");
+            Console.WriteLine(temp);
+            Console.WriteLine("---------------------");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int valg = Velkomst();
+
+            int resultat = 0;
+
+            switch (valg)
+            {
+                case 1:
+                    resultat = Plus();
+                    break;
+                case 2:
+                    resultat =  Minus();
+                    break;
+                case 3:
+                    resultat =  Gange();
+                    break;
+                case 4:
+                    resultat =  Dividere();
+                    break;
+
+                default:
+                    break;
+            }
+
+            UdskrivResultat(resultat);
+        }
+    }
+}

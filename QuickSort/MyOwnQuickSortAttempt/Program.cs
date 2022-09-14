68df30129ada8ac75cf38f1221b9b18f9333896b
Bjoern <bjoe6880@elev.techcollege.dk>
2022-09-14 10:01:47 +0200

    Oprettet initial code til at implementere quicksort. Array med random tal der skal sorteres og vises på konsol

--- QuickSort/MyOwnQuickSortAttempt/Program.cs
+++ QuickSort/MyOwnQuickSortAttempt/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!"); //Test comment for git
+            int antalTal = 12;                      //variabel til at bestemme størrelse af tal-array der skal sorteres
+            Console.WriteLine("=== Bjørns Quick-Sort ===");
+            
+            int[] arrTal = new int[antalTal];       //Oprett array til at holde tal der skal sorteres
+
+            Random rng = new Random();              //Ny random nummer objekt
+
+            for (int i = 0; i < antalTal; i++)               //fylder arrayet op med random tal
+            {
+                arrTal[i] = rng.Next(1,101);        //Fylder et random tal imelle 1 og 100 in i en plads på arrayet
+            }
+
+            //Skriver de autogenerede tal ud på konsollen
+            Console.Write("Tal at sortere: ");
+            foreach (int i in arrTal)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine();                    //Linjeskift
+
+
         }
     }
 }
